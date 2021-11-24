d39554d0674b3497b757b321d91d0c142d4275e5
enditidev <87295058+enditidev@users.noreply.github.com>
2021-11-24 16:58:00 +0700

    Delete CHAMCONGVN directory

--- CHAMCONGVN/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CHAMCONGVN.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- CHAMCONGVN/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CHAMCONGVN.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
